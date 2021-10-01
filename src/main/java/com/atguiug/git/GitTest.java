d91544cc5db4e737156be53a1323b37425002de9
Liubin <Liubin@atguigu.com>
2021-10-01 21:31:08 +0800

    hot-fix commit

--- src/main/java/com/atguiug/git/GitTest.java
+++ src/main/java/com/atguiug/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("修改了");
         System.out.println("我又修改了一些内容");
+        System.out.println("hot-fix");
     }
 }
