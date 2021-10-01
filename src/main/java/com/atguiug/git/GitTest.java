21c1aa6aab1a98b74e8d3d94deeed2d3d010b60f
Liubin <Liubin@atguigu.com>
2021-10-01 21:19:17 +0800

    second commit

--- src/main/java/com/atguiug/git/GitTest.java
+++ src/main/java/com/atguiug/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("修改了");
+        System.out.println("我又修改了一些内容");
     }
 }
